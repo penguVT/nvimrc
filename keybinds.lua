8092aafd6b597a891c6090918580b8191f91f4f6
penguNu <penguinudev@gmail.com>
2024-06-17 20:22:56 -0400

    Comment out compilation command

--- keybinds.lua
+++ keybinds.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<leader>ff", teleBuiltins.find_files, {})
 vim.keymap.set("n", "<leader>fg", teleBuiltins.live_grep, {})
 
 -- compile current directory
-vim.keymap.set("n", "<leader>cc", "<cmd>!compiler %:p<cr>")
+--vim.keymap.set("n", "<leader>cc", "<cmd>!compiler %:p<cr>")
 
 vim.keymap.set("n", "<leader>mg", function()
   require("neogit").open()
@@ -15,3 +15,6 @@ end)
 -- terminal
 vim.keymap.set("n", "<leader>pt", vim.cmd.term)
 vim.keymap.set("t", "<leader>dt", "<C-\\><C-n><CR>")
+
+-- preview
+vim.keymap.set("n", "<leader>pp", "<cmd>!sent '%'<cr>")
