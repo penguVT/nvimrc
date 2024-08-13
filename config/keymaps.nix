573eb8de737df50544c62dc530cac22f10d9ff99
penguVT <penguinudev@gmail.com>
2024-08-13 18:35:58 -0400

    Move Telescope keymaps to plugin file

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -18,22 +18,5 @@
       key = "<leader>dt";
       action = "<C-\\><C-n><cr>";
     }
-
-    # telescope
-    {
-      mode = "n";
-      key = "<leader>ff";
-      action = "<cmd>Telescope find_files<cr>";
-    }
-    {
-      mode = "n";
-      key = "<leader>fg";
-      action = "<cmd>Telescope live_grep<cr>";
-    }
-    {
-      mode = "n";
-      key = "<leader>hh";
-      action = "<cmd>Telescope help_tags<cr>";
-    }
   ];
 }
--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -2,4 +2,22 @@
   plugins.telescope = {
     enable = true;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ff";
+      action = "<cmd>Telescope find_files<cr>";
+    }
+    {
+      mode = "n";
+      key = "<leader>fg";
+      action = "<cmd>Telescope live_grep<cr>";
+    }
+    {
+      mode = "n";
+      key = "<leader>hh";
+      action = "<cmd>Telescope help_tags<cr>";
+    }
+  ];
 }
