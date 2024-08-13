6c39dab0b3e803b957285d48cb6151d407efb1fc
penguVT <penguinudev@gmail.com>
2024-08-13 18:34:29 -0400

    Add missing keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,5 +1,25 @@
 {
   keymaps = [
+    # file explorer
+    {
+      mode = "n";
+      key = "<leader>pf";
+      action = "<cmd>Ex<cr>";
+    }
+
+    # terminal
+    {
+      mode = "n";
+      key = "<leader>pt";
+      action = "<cmd>term<cr>";
+    }
+    {
+      mode = "t";
+      key = "<leader>dt";
+      action = "<C-\\><C-n><cr>";
+    }
+
+    # telescope
     {
       mode = "n";
       key = "<leader>ff";
@@ -9,6 +29,11 @@
       mode = "n";
       key = "<leader>fg";
       action = "<cmd>Telescope live_grep<cr>";
+    }
+    {
+      mode = "n";
+      key = "<leader>hh";
+      action = "<cmd>Telescope help_tags<cr>";
     }
   ];
 }
